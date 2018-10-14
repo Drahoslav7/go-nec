7df57077a21d060aaaebc3ce96af48a8b322d8b3
Drahoslav <drahoslav@yo2.cz>
2018-10-15 00:39:35 +0200

    Add test cases

--- nec_test.go
+++ nec_test.go
@@ -2,6 +2,7 @@ package nec
 
 import (
 	"fmt"
+	"testing"
 )
 
 func ExampleEncode() {
@@ -28,4 +29,18 @@ func ExampleSignal_Transmit() {
 	fmt.Println()
 	sig.TransmitRepeat(connect, 4)
 	fmt.Println()
+}
+
+func TestEncode(t *testing.T) {
+	s := Encode(0, 1)
+	if s.String()[:128] != "11111111111111110000000010101010101010101000100010001000100010001000100010001010101010101010100010001000100010001000100010000000" {
+		t.Error("Signal is not Encoded correctly - nec")
+	}
+}
+
+func TestEncodeExt(t *testing.T) {
+	s := EncodeExt(61184, 3)
+	if s.String()[:128] != "11111111111111110000000010101010101010101000100010001000101000100010001000100010101010101010101000100010001000100010001000000000" {
+		t.Error("Signal is note Encoded correctly - Extended nec")
+	}
 }
